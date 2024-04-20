7e8cb2dcdb519b1c51523e2b74ce96095bffd6a5
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-20 17:27:20 +0800

    fix

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header>
             <nav className="mx-auto flex max-w-7xl justify-between p-6" aria-label="Global">
                 <div className="flex">
-                    <a href="would-you-rather.com" className="-m-1.5 p-1.5">
+                    <a href="/" className="-m-1.5 p-1.5">
                         <img className="h-20 w-auto" src="logo.png" alt="" />
                     </a>
                 </div>
